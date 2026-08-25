da7c7efa7d9d92392f75bea0e011dbb4468d890f
dominic.koh <dominic.koh@bytedance.com>
2026-08-25 14:38:28 +0800

    Fix Leaflet map tile provider
    Change-Id: I744e1f215a8c5adf7bfbfba755c410bbd602a6e4

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -95,10 +95,9 @@
         scrollWheelZoom: false // prevents zooming when scrolling down the page
       }).setView([1.3521, 103.8198], 11); // Center of Singapore
 
-      L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 20
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 19
       }).addTo(leafletMap);
     }
 
